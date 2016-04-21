41edef61037b3ceb370ffab6dee3053404204c30
Aditia Mahdar <adit@41studio.com>
2016-04-21 21:11:01 +0700

    Added validation

--- app/models/simple_enquiry/enquiry.rb
+++ app/models/simple_enquiry/enquiry.rb
@@ -3,7 +3,10 @@ module SimpleEnquiry
     belongs_to :sender, polymorphic: true
     belongs_to :owner, polymorphic: true
     belongs_to :object, polymorphic: true
+    delegate :name, to: :object, prefix: true, allow_nil: true
 
     has_many :messages, dependent: :destroy
+
+    validates :title, :object, :owner, :sender, presence: true
   end
 end
