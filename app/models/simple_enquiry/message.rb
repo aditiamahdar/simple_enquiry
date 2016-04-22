db6220d7a0c5a33398e97baa4b3e32a3ca88c6a2
Aditia Mahdar <adit@41studio.com>
2016-04-22 22:08:52 +0700

    Added validation for message

--- app/models/simple_enquiry/message.rb
+++ app/models/simple_enquiry/message.rb
@@ -2,5 +2,7 @@ module SimpleEnquiry
   class Message < ActiveRecord::Base
     belongs_to :enquiry
     belongs_to :sender, polymorphic: true
+
+    validates :body, :enquiry, :sender, presence: true
   end
 end
